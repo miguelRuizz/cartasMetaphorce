c081b7ff3965e8e65c0b9747dd2e8630dca98a93
profitamoleRuiz <20030935@itcelaya.edu.mx>
2024-06-01 23:16:40 -0600

    Algoritmo listo e implementado. Tiene ciertos detalles, pero ya sigue la regla general de conseguir el puntaje de 21 y de que los ases pueden valer 1 u 11.

--- src/main/java/com/miguel/cartasmetaphorce/CartasMetaphorce.java
+++ src/main/java/com/miguel/cartasmetaphorce/CartasMetaphorce.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
 import java.util.Stack;
+import java.util.Scanner;
 
 public class CartasMetaphorce {
     
@@ -11,12 +12,13 @@ public class CartasMetaphorce {
     static Stack<Carta> pilaReserva = new Stack();
     static Jugador jug1 = new Jugador("Gustavo");
     static Jugador jug2 = new Jugador("Cecilia");
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         crearMazo();
         mezclarMazoReserva();
         muestraMazo(Mazo);
-        System.out.println("");
+        System.out.println(",");
         llenarPilas();
         iniciaJuego();
     }
@@ -39,7 +41,7 @@ public class CartasMetaphorce {
     
     public static void muestraMazo(List<Carta> mazo){
         for(int i=0; i<=mazo.size()-1 ; i++){//Palos    
-            System.out.println("Indice: " + i +
+            System.out.println("Carta No.: " + i +
                                 " Palo: " + mazo.get(i).getPalo() + 
                                 " Valor: " + mazo.get(i).getValor() + ".");
         }
@@ -60,32 +62,59 @@ public class CartasMetaphorce {
         }     
     }
     
-    public static void tomarCarta(List<Carta> mazoDestino){
+    public static void tomarCarta(List<Carta> mazoDestino, String jugador){
+        int as = 0;
         Carta cartaJugador = pilaReserva.pop();
+        if(cartaJugador.getValor() == 1){//Evalúa si tiene un As y pregunta si quiere cambiar su valor
+            System.out.println(jugador + ", tienes un as, " +
+                    "¿qué valor quieres darle? puedes elegir 1 u 11.");
+            as = sc.nextInt();
+            if(as == 1){
+                cartaJugador.setValor(1);
+            }else{
+                cartaJugador.setValor(11);
+            }
+        }
         mazoDestino.add(cartaJugador);
     }
     
     public static void iniciaJuego(){
+        String respuesta = "";
+        do {
+            iniciarRonda();
+            System.out.println("¿Quieres continuar a una nueva ronda?");
+            respuesta = sc.nextLine();
+            tomarCarta(jug1.mazo, jug1.getNombre());
+            tomarCarta(jug2.mazo, jug2.getNombre());
+        } while (respuesta.equalsIgnoreCase("Si")
+                || respuesta.equalsIgnoreCase("s")
+                || respuesta.equalsIgnoreCase(""));
+        conteoValores(jug1.getPuntuaje(), jug1.getNombre(), jug2.getPuntuaje(), jug2.getNombre(), true);
+    }
+
+    public static void iniciarRonda(){
         int conteo = 0;
         System.out.println("Mazo de " + jug1.getNombre());
         muestraMazo(jug1.mazo);
-        for (Carta carta : jug1.mazo) { //Se llena la pila de reserva
+        for (Carta carta : jug1.mazo) {
             conteo += carta.getValor();
         }
-        
+
         int conteo2 = 0;
         System.out.println("Mazo de " + jug2.getNombre());
         muestraMazo(jug2.mazo);
-        for (Carta carta : jug2.mazo) { //Se llena la pila de reserva
+        for (Carta carta : jug2.mazo) {
             conteo2 += carta.getValor();
         }
+        jug1.setPuntuaje(conteo);
+        jug2.setPuntuaje(conteo2);
+        conteoValores(jug1.getPuntuaje(), jug1.getNombre(), jug2.getPuntuaje(), jug2.getNombre(), false);
 
-        conteoValores(conteo, jug1.getNombre(), conteo2, jug2.getNombre());
     }
     
-    public static void conteoValores(int conteoj1, String jugador1, int conteoj2, String jugador2){
+    public static void conteoValores(int conteoj1, String jugador1, int conteoj2, String jugador2, boolean fin){
         if(conteoj1 == 21){
-            System.out.println("El puntaje de " + jugador1 + " es: " + conteoj1 + ".");
+            System.out.println("El puntaje de " + jugador1 + " es exactamente " + conteoj1 + ".");
             System.out.println("¡Felicidades, ganó " + jugador1 + "!");
             System.exit(0);
         } else if (conteoj1 > 21) {
@@ -95,7 +124,7 @@ public class CartasMetaphorce {
         }
 
         if(conteoj2 == 21){
-            System.out.println("El puntaje de " + jugador2 + " es: " + conteoj2 + ".");
+            System.out.println("El puntaje de " + jugador2 + " es exactamente " + conteoj2 + ".");
             System.out.println("¡Felicidades, ganó " + jugador2 + "!");
             System.exit(0);
         } else if (conteoj2 > 21) {
@@ -103,11 +132,12 @@ public class CartasMetaphorce {
             System.out.println("Por lo tanto, ¡felicidades, ganó " + jugador1 + "!");
             System.exit(0);
         }
-        else {
+
+        if(fin==true) {
             if(conteoj1 > conteoj2){
-                System.out.println(jugador1 + " tuvo un puntaje más acercado a 21. ¡" + jugador1 + " ganó!");
+                System.out.println(jugador1 + " tuvo un puntaje más acercado a 21 con " + conteoj1 + ". ¡" + jugador1 + " ganó!");
             } else {
-                System.out.println(jugador2 + " tuvo un puntaje más acercado a 21. ¡" + jugador2 + " ganó!");
+                System.out.println(jugador2 + " tuvo un puntaje más acercado a 21 con " + conteoj2 + ". ¡" + jugador2 + " ganó!");
             }
         }
     }
